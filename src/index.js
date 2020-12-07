8866710e8e1e5e48a13b27a14ee89c3db7972bad
Johnny Oshika <johnnyoshika@gmail.com>
2020-12-07 14:18:02 -0800

    Cleans Sendgrid

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
 import 'dotenv/config';
+import { Pool } from 'pg';
+import faker from 'faker';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const pool = new Pool({
+  database: process.env.DATABASE,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT, 10),
+});
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
+(async function () {})();
 
-console.log(user);
+async function cleansSendgrid() {
+  const hooks = `
+    SELECT id, body, body_json
+    FROM public.webhooks
+    where endpoint_id = $1;
+  `;
 
-console.log(process.env.SOME_ENV_VARIABLE);
+  const { rows } = await pool.query(hooks, [2]);
+
+  for (const row of rows) {
+    console.log(
+      `----------------------------- NEW ROW: ${row.id}-----------------------------`,
+    );
+    console.log(row);
+    const email = faker.internet.email();
+    row.body = row.body.replace(
+      /("email":")(.*?)(")/g,
+      `$1${email}$3`,
+    );
+
+    for (const jsonItem of row.body_json)
+      if (jsonItem.email) jsonItem.email = email;
+
+    console.log('');
+    console.log('Cleansed:');
+    console.log('');
+    console.log(row);
+
+    await pool.query(
+      `
+        UPDATE webhooks
+        SET
+          body = $1,
+          body_json = $2
+        WHERE id = $3
+      `,
+      [row.body, JSON.stringify(row.body_json), row.id],
+    );
+  }
+}
