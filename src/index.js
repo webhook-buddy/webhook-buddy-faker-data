c79e6ff5e996d867056f4c33325454bdf86289bb
Johnny Oshika <johnnyoshika@gmail.com>
2020-12-08 09:43:38 -0800

    Scramble more Mailgun data

--- src/index.js
+++ src/index.js
@@ -78,6 +78,13 @@ async function cleansMailgun() {
     );
     console.log(row);
     const email = faker.internet.exampleEmail();
+    const domain = email.split('@')[1];
+    const from = 'do-not-reply@acme.org';
+    const subject = faker.lorem.sentence(4).replace('.', '');
+    const key = faker.random.uuid().replace(/-/g, '');
+    const url = `https://se.api.mailgun.net/v3/domains/mg.acme.org/messages/${key}`;
+    const ip = faker.internet.ip();
+
     row.body = [
       row.body,
       'targets',
@@ -90,12 +97,56 @@ async function cleansMailgun() {
       ),
     );
 
+    row.body = [row.body, 'from', 'sender'].reduce((acc, cur) =>
+      acc.replace(
+        new RegExp(`("${cur}": ")(.*?)(",)`, 'g'),
+        `$1${from}$3`,
+      ),
+    );
+
+    row.body = row.body.replace(
+      /("subject": ")(.*?)(")/g,
+      `$1${subject}$3`,
+    );
+
+    row.body = row.body.replace(
+      /("sending-ip": ")(.*?)(")/g,
+      `$1${ip}$3`,
+    );
+
+    row.body = row.body.replace(/("key": ")(.*?)(")/g, `$1${key}$3`);
+    row.body = row.body.replace(/("url": ")(.*?)(")/g, `$1${url}$3`);
+
+    row.body = row.body.replace(
+      /("mx-host": ")(.*?)(")/g,
+      `$1smtp.${domain}$3`,
+    );
+
+    row.body = row.body.replace(
+      /("recipient-domain": ")(.*?)(")/g,
+      `$1${domain}$3`,
+    );
+
     if (
       row.body_json['event-data'] &&
       row.body_json['event-data'].envelope &&
       row.body_json['event-data'].envelope.targets
     )
       row.body_json['event-data'].envelope.targets = email;
+
+    if (
+      row.body_json['event-data'] &&
+      row.body_json['event-data'].envelope &&
+      row.body_json['event-data'].envelope.sender
+    )
+      row.body_json['event-data'].envelope.sender = from;
+
+    if (
+      row.body_json['event-data'] &&
+      row.body_json['event-data'].envelope &&
+      row.body_json['event-data'].envelope['sending-ip']
+    )
+      row.body_json['event-data'].envelope['sending-ip'] = ip;
 
     if (
       row.body_json['event-data'] &&
@@ -107,9 +158,54 @@ async function cleansMailgun() {
 
     if (
       row.body_json['event-data'] &&
+      row.body_json['event-data'].message &&
+      row.body_json['event-data'].message.headers &&
+      row.body_json['event-data'].message.headers.from
+    )
+      row.body_json['event-data'].message.headers.from = from;
+
+    if (
+      row.body_json['event-data'] &&
+      row.body_json['event-data'].message &&
+      row.body_json['event-data'].message.headers &&
+      row.body_json['event-data'].message.headers.subject
+    )
+      row.body_json['event-data'].message.headers.subject = subject;
+
+    if (
+      row.body_json['event-data'] &&
       row.body_json['event-data'].recipient
     )
       row.body_json['event-data'].recipient = email;
+
+    if (
+      row.body_json['event-data'] &&
+      row.body_json['event-data'].storage &&
+      row.body_json['event-data'].storage.key
+    )
+      row.body_json['event-data'].storage.key = key;
+
+    if (
+      row.body_json['event-data'] &&
+      row.body_json['event-data'].storage &&
+      row.body_json['event-data'].storage.url
+    )
+      row.body_json['event-data'].storage.url = url;
+
+    if (
+      row.body_json['event-data'] &&
+      row.body_json['event-data']['delivery-status'] &&
+      row.body_json['event-data']['delivery-status']['mx-host']
+    )
+      row.body_json['event-data']['delivery-status'][
+        'mx-host'
+      ] = `smtp.${domain}`;
+
+    if (
+      row.body_json['event-data'] &&
+      row.body_json['event-data']['recipient-domain']
+    )
+      row.body_json['event-data']['recipient-domain'] = domain;
 
     if (row.headers['Content-Length'])
       row.headers['Content-Length'] = row.body.length + '';
@@ -119,7 +215,6 @@ async function cleansMailgun() {
     console.log('');
     console.log(row);
 
-    // Stringify is not necessary for body_json here b/c it's an object, but left it in here for consistency with cleansSendgrid()
     await pool.query(
       `
         UPDATE webhooks
@@ -129,7 +224,7 @@ async function cleansMailgun() {
           headers = $3
         WHERE id = $4
       `,
-      [row.body, JSON.stringify(row.body_json), row.headers, row.id],
+      [row.body, row.body_json, row.headers, row.id],
     );
   }
 }
